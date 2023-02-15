63fd9340ad12ea355bd144c8fbc7ccc8e24f8e97
Anna Tarankova <tarankovaanna@gmail.com>
2023-02-15 15:57:59 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,87 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int sumSales(long[] sales) {
+        long sumMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sumMonth = sumMonth + sales[i];
+        }
+        return (int) sumMonth;
+    }
+
+    public int avgSumSales(long[] sales) {
+        int avgSumMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            avgSumMonth = (int) (avgSumMonth + sales[i]);
+
+        }
+        return avgSumMonth / sales.length;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int countMonthWithMinAvg(long[] sales) {
+
+        int count = 0;
+        int avgSumMonth = 0;
+
+
+        for (int i = 0; i < sales.length; i++) {
+            avgSumMonth = (int) (avgSumMonth + sales[i]);
+
+        }
+
+        for (int i = 0; i < sales.length; i++) {
+            if (avgSumMonth / sales.length > sales[i]) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
+    public int countMonthWitMoreAvg(long[] sales) {
+
+        int count = 0;
+        int avgSumMonth = 0;
+
+
+        for (int i = 0; i < sales.length; i++) {
+            avgSumMonth = (int) (avgSumMonth + sales[i]);
+
+        }
+
+        for (int i = 0; i < sales.length; i++) {
+            if (avgSumMonth / sales.length < sales[i]) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+}
+
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,67 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void sumSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual = service.sumSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void avgSumTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.avgSumSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.maxSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.minSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countMonthWithLessAvgTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.countMonthWithMinAvg(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countMonthWithMoreAvgTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.countMonthWitMoreAvg(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
